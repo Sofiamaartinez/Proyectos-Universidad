b0f6e7f6f41b63ac0b5497be991feac8aa32b171
sofia <73569562+Sofiamaartinez@users.noreply.github.com>
2024-01-20 18:14:10 -0600

    Add files via upload

---
+++ Tarea-raiz/Tarea-raiz/Program-raiz.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace RaizCuadradaCubica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            Console.WriteLine("Escribe un numero para sacar su raiz: ");
+            num1= int.Parse(Console.ReadLine());
+            double raizCuadrada = Math.Sqrt(num1);
+            Console.WriteLine("La raíz cuadrada de {0} es {1}", num1, raizCuadrada);
+        }
+    }
+}
